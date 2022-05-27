aede119cdce83ff6ad4b0e30584b3ff737b37781
jeong922 <hwajeong922@naver.com>
2022-05-27 15:52:03 +0900

    localStorage 기능 추가

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const items = document.querySelector('.items');
 const input = document.querySelector('.footer__input');
 const form = document.querySelector('.new-form');
 
+let shoppingListArray = [];
+
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   onAdd();
@@ -10,36 +12,49 @@ form.addEventListener('submit', (event) => {
 
 function onAdd() {
   // 1. 입력한 텍스트 받아오기
-  const text = input.value;
-  if (text === '') {
+  const text = { id: Date.now(), name: input.value };
+  shoppingListArray.push(text);
+  if (text.name === '') {
     input.focus();
     return;
   }
   // 2. 새로운 item 생성(텍스트 + 삭제 버튼)
   const item = createItem(text);
   // 3. items 컨테이너안에 새로 만든 아이템 추가
-  items.appendChild(item);
+  //items.appendChild(item);
   // 4. 새로 추가된 아이템으로 스크롤링
   item.scrollIntoView({ block: 'center' });
   // 5. input 초기화(input value 비우기, input focus)
   input.value = '';
   input.focus();
+  // 로컬스토리지 저장
+  saveLocalStorage();
 }
-let id = 0; // UUID
+
+// 로컬스토리지 저장
+function saveLocalStorage() {
+  const stringifyName = JSON.stringify(shoppingListArray);
+  localStorage.setItem('itemName', stringifyName);
+}
+
+// 로컬스토리지에서 값 받아옴
+const getLocalStorage = localStorage.getItem('itemName');
+
 function createItem(text) {
+  console.log(text);
   const itemRow = document.createElement('li');
   itemRow.setAttribute('class', 'item__row');
-  itemRow.setAttribute('data-id', id);
+  itemRow.setAttribute('data-id', text.id);
   itemRow.innerHTML = `
           <div class="item">
-            <span class="item__name">${text}</span>
+            <span class="item__name">${text.name}</span>
             <button class="item__delete">
-              <i class="fas fa-trash" data-id=${id}></i>
+              <i class="fas fa-trash" data-id=${text.id}></i>
             </button>
           </div>
           <div class="item__divider"></div>
   `;
-  id++;
+  items.appendChild(itemRow);
   return itemRow;
 }
 
@@ -48,5 +63,16 @@ items.addEventListener('click', (event) => {
   if (id) {
     const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
     toBeDeleted.remove();
+    shoppingListArray = shoppingListArray.filter(
+      (item) => item.id !== parseInt(id)
+    );
+    console.log(shoppingListArray);
+    saveLocalStorage();
   }
 });
+
+if (getLocalStorage !== null) {
+  const parseName = JSON.parse(getLocalStorage);
+  shoppingListArray = parseName;
+  parseName.forEach(createItem);
+}
